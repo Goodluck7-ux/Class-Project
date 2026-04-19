b9b3a187d58fd446948b6a023864c9336301fd6f
unknown <wodibrenda26@gmail.com>
2026-04-20 01:03:52 +0200

    message

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,12 @@
+import ShopSection from "@/Component/ShopSection";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <h2>Edited by obed</h2>
+    <div>
+      <ShopSection/>
+    </div>
+ 
   );
 }
